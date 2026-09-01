9378142e81208775f0207df1387cf4fad7a4403e
finding-haseeb <144657162+finding-haseeb@users.noreply.github.com>
2026-09-01 17:35:57 +0530

    Time: 7 ms (35.99%) | Memory: 28.4 MB (64.08%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        stack <int> st;
+        vector<int> ans( n , -1);
+
+        for (int i =2* n-1; i >= 0; i--) {
+            int index = i %n;
+            int currentelement = nums[index];
+
+            while( !st.empty() && st.top() <= currentelement) {
+                st.pop();
+            }
+            if ( i< n ) {
+                if( st.empty()) {
+                    ans[index] = -1;
+                }
+                else {
+                    ans[index ] = st.top();
+                }
+            }
+            st.push( currentelement );
+        }
+        return ans;
+    }
+};
