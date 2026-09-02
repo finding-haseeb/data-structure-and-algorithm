194825d09e4b80461e6fee5eda6a654899341a42
finding-haseeb <144657162+finding-haseeb@users.noreply.github.com>
2026-09-02 15:48:50 +0530

    Time: 12 ms (26.05%) | Memory: 23 MB (58.72%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+
+        int low = 1;
+        int high = *max_element( piles.begin() , piles.end());
+        int ans=0;
+
+        while ( low <= high) {
+            int mid = low + ( high - low )/2;
+            
+            long long sum =0;
+            for ( int i =0; i < n ; i++) {
+                sum = sum + ((piles[i] -1)/mid) + 1;
+            }
+
+            if ( sum <= h) {
+                ans = mid;
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
