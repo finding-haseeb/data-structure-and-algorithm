bb6d8476c9fad1778e7daf112edcb4281a4145c8
finding-haseeb <144657162+finding-haseeb@users.noreply.github.com>
2026-09-02 12:56:28 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (37.81%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+
+        int n = nums.size();
+
+        int low = 0;
+        int high = n - 1;
+
+        // Edge cases
+        if (n == 1)
+            return nums[0];
+
+        if (nums[0] != nums[1])
+            return nums[0];
+
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            // Single element found
+            if (nums[mid] != nums[mid - 1] &&
+                nums[mid] != nums[mid + 1]) {
+
+                return nums[mid];
+            }
+
+            // We are on the LEFT side of single element
+            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) ||
+                (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+
+                low = mid + 1;
+            }
+
+            // We are on the RIGHT side
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
