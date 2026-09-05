a2a96a3a5471175e6a5258fa6efa70e8e14e2958
finding-haseeb <144657162+finding-haseeb@users.noreply.github.com>
2026-09-05 17:42:37 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (41.72%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int func(TreeNode* node,  int & diameter ) {
+        if(node == nullptr) {
+            return 0;
+        }
+        int lh = func(node -> left, diameter);
+        int rh = func(node -> right , diameter);
+
+        diameter = max(diameter, lh+ rh);
+
+        return 1 + max(lh, rh );
+
+
+     }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter =0;
+        func( root, diameter );
+        return diameter;
+
+        
+    }
+};
