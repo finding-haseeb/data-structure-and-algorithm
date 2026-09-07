1eab272e7ee21968d16671a39daad4c2f2159e1a
finding-haseeb <144657162+finding-haseeb@users.noreply.github.com>
2026-09-07 23:56:44 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (17.12%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,32 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+    void inorder( TreeNode* root , vector<int>& ans ) {
+        if ( root == nullptr) return ;
+
+        inorder ( root -> left , ans);
+        ans.push_back(root -> val);
+        inorder( root -> right , ans);
+    } 
 public:
-
-    int count = 0;
-
-    void inorder(TreeNode* root, int k, int &ans) {
-
-        if (root == nullptr) return;
-
-        inorder(root->left, k, ans);
-
-        if (count >= k) return;
-
-        count++;
-
-        if (count == k) {
-            ans = root->val;
-            return;
-        }
-
-        inorder(root->right, k, ans);
-    }
-
     int kthSmallest(TreeNode* root, int k) {
+        vector<int> ans;
+        inorder( root, ans );
 
-        int ans = -1;
-
-        inorder(root, k, ans);
-
-        return ans;
+        int smallest = ans[k-1];
+        return smallest;
+        
     }
 };
